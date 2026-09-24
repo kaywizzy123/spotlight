49b61f910818a2da7575c30b116c3be2d0934d8b
Oluwakayode Ogunremi <kayodeogunremi33@gmail.com>
2026-09-23 17:38:29 -0700

    started the create post page

--- src/app/(tabs)/create.tsx
+++ src/app/(tabs)/create.tsx
@@ -1,10 +1,41 @@
-import { Text, View } from "react-native";
-import { styles } from "../../styles/auth.styles";
+import { COLORS } from "@/constants/theme";
+import { useUser } from "@clerk/expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../../styles/create.styles";
 
 export default function Create() {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [caption, setCaption] = useState("");
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [isSharing, setIsSharing] = useState(false);
+
+  if (!selectedImage) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={28} color={COLORS.primary} />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>New Post</Text>
+          <View style={{ width: 28 }} />
+        </View>
+
+        <TouchableOpacity style={styles.emptyImageContainer}>
+          <Ionicons name="image-outline" size={48} color={COLORS.grey} />
+          <Text style={styles.emptyImageText}>Tap to select an image</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Create</Text>
+      <Text>Create</Text>
     </View>
   );
 }
